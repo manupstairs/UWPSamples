784b35017aa710c70de6be2153c2780f2de077bd
xingzheng <xingzheng@hotmail.com>
2016-05-14 21:23:00 +0800

    memery leak check sample.

---
+++ UWPSamples/EventMemoryLeak/FakeService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventMemoryLeak
+{
+    public class FakeService
+    {
+        public static FakeService Instance = new FakeService();
+
+        public event EventHandler ShowMeTheMoneyEvent;
+
+        private FakeService() { }
+    }
+}
---
+++ UWPSamples/EventMemoryLeak/SecondPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace EventMemoryLeak
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SecondPage : Page
+    {
+        public ArrayList arrayList { get; set; }
+
+        public SecondPage()
+        {
+            this.InitializeComponent();
+            arrayList = new ArrayList(10000000);
+            FakeService.Instance.ShowMeTheMoneyEvent += Instance_ShowMeTheMoneyEvent;
+        }
+
+        private void Instance_ShowMeTheMoneyEvent(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.GoBack();
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            FakeService.Instance.ShowMeTheMoneyEvent -= Instance_ShowMeTheMoneyEvent;
+        }
+    }
+}
