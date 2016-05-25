2cf06fed6cfe8e6d75d6e1c6a237a8f11b866bdb
xingzheng <xingzheng@hotmail.com>
2016-05-25 22:07:51 +0800

    Adaptive UserControl

---
+++ UWPSamples/AdaptiveUserControl/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AdaptiveUserControl
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public List<int> TestList
+        {
+            get; set;
+        }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            TestList = new List<int>();
+            for (int i = 0; i < 1000; i++)
+            {
+                TestList.Add(i);
+            }
+        }
+    }
+}
