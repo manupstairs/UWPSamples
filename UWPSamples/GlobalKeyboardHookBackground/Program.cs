c555517840b434e6489272971d31a586568c94a0
xingzheng <xingzheng@outlook.com>
2020-09-20 17:42:24 +0800

    Init global keboard hook projects

---
+++ UWPSamples/GlobalKeyboardHookBackground/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalKeyboardHookBackground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
