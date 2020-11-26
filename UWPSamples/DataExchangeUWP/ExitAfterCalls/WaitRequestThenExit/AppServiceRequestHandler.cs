1a81f09862414363d1b2e089c9ed37f248329026
xingzheng <xingzheng@outlook.com>
2020-11-26 22:40:43 +0800

    Init ExitAfterHandleRequest

---
+++ UWPSamples/DataExchangeUWP/ExitAfterCalls/WaitRequestThenExit/AppServiceRequestHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.AppService;
+using Windows.Foundation.Collections;
+
+namespace WaitRequestThenExit
+{
+    class AppServiceRequestHandler
+    {
+        private AppServiceConnection Connection { get; set; }
+
+        public Task InitializeTask { get; private set; }
+
+        public AppServiceRequestHandler()
+        {
+            InitializeTask = InitializeAsync();
+        }
+
+        public async Task InitializeAsync()
+        {
+            Connection = new AppServiceConnection();
+            Connection.PackageFamilyName = Package.Current.Id.FamilyName;
+            Connection.AppServiceName = "NotificationAppService";
+            AppServiceConnectionStatus status = await Connection.OpenAsync();
+            if (status != AppServiceConnectionStatus.Success)
+            {
+                Console.WriteLine(status);
+            }
+            else
+            {
+                Console.WriteLine(status);
+                Connection.RequestReceived += Connection_RequestReceived;
+            }
+        }
+
+        private void Connection_RequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        {
+            var content = args.Request.Message["request"].ToString();
+            Process.Start(content);
+        }
+    }
+}
---
+++ UWPSamples/DataExchangeUWP/ExitAfterHandleRequest/OnewayRequest.FrontUWP/AppServiceConnectionConnectedEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.AppService;
+
+namespace OnewayRequest.FrontUWP
+{
+    public class AppServiceConnectionConnectedEventArgs : EventArgs
+    {
+        public AppServiceConnection Connection { get; }
+
+        public AppServiceConnectionConnectedEventArgs(AppServiceConnection connection)
+        {
+            Connection = connection;
+        }
+    }
+}
